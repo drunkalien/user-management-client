699df74ab877e1b19124ec813b500bbc4664e111
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-06-04 17:18:26 +0500

    services

---
+++ src/services/api/request.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosError } from "axios";
+
+export const requestInstance = axios.create({
+  baseURL: "http://localhost:5000",
+});
+
+requestInstance.interceptors.request.use((config: any) => {
+  const token = window.localStorage.getItem("token");
+
+  config.headers["Authorization"] = `Bearer ${token}`;
+
+  return config;
+});
+
+requestInstance.interceptors.request.use(
+  (config: any) => config,
+  (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      // TODO should redirect to home page
+      console.log("Not authorized");
+    }
+  }
+);
+
+export default requestInstance;
